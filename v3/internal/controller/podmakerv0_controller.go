372ea9a0e63f255583f15b6591ef3a2d04fe4774
root <root@DESKTOP-L3SM155.localdomain>
2025-08-30 23:45:30 +0530

    update

--- v3/internal/controller/podmakerv0_controller.go
+++ v3/internal/controller/podmakerv0_controller.go
@@ -53,7 +53,7 @@ type PodMakerv0Reconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
 func (r *PodMakerv0Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	// _ = logf.FromContext(ctx)
+	_ = logf.FromContext(ctx)
 
 	// TODO(user): your logic here
 
