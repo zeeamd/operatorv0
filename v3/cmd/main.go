5510bbccd85f3bb7d8602269ef5dfef76a0043ac
root <root@ip-10-142-110-130.ec2.internal>
2025-08-31 10:06:35 +0000

    add deploy maker

--- v3/cmd/main.go
+++ v3/cmd/main.go
@@ -209,6 +209,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "PodMakerv0")
 		os.Exit(1)
 	}
+	if err := (&controller.DeployMakerv0Reconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DeployMakerv0")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
